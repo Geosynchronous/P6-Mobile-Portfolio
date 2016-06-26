546df5af054b2377c563611d90791c0b21e7aa24
George Fischer <geosynchronous@mac.com>
2016-06-25 21:31:09 -0600

    fix: FIX 16 revert some code to original
    The code for determineDX and changePizzaSizes
    to hopefully restore full UI/UX functionality

--- views/js/main.js
+++ views/js/main.js
@@ -450,38 +450,75 @@ var resizePizzas = function(size) {
 
     changeSliderLabel(size);
 
-    // FIX 11
-    // Major refactor to use style 'transform scale'
-    // Replace style 'width' change
-    // Adjusted percentages in 'sizeSwithcer'
-    function determineNewScale(size) {
-        // Changes the slider value to a percent width
-        function sizeSwitcher(size) {
-            switch (size) {
-                case "1":
-                    return 0.5;
-                case "2":
-                    return 0.6666;
-                case "3":
-                    return 1.0;
-                default:
-                    console.log("bug in sizeSwitcher");
-            }
-        }
-        return sizeSwitcher(size);
+    // // FIX 11
+    // // Major refactor to use style 'transform scale'
+    // // Replace style 'width' change
+    // // Adjusted percentages in 'sizeSwithcer'
+    // function determineNewScale(size) {
+    //     // Changes the slider value to a percent width
+    //     function sizeSwitcher(size) {
+    //         switch (size) {
+    //             case "1":
+    //                 return 0.5;
+    //             case "2":
+    //                 return 0.6666;
+    //             case "3":
+    //                 return 1.0;
+    //             default:
+    //                 console.log("bug in sizeSwitcher");
+    //         }
+    //     }
+    //     return sizeSwitcher(size);
+    // }
+
+    // // READ and WRITE seperate tasks to avoid FSL
+    // function changePizzaSizes(size) {
+    //     // READ - Load all pizza image elements
+    //     var element = document.querySelectorAll(".img-responsive");
+    //     // Sets value of newScale
+    //     var newScale = determineNewScale(size);
+    //     // WRITE - New transforms for pizza size updated here
+    //     for (var i = 0; i < element.length; i++) {
+    //         element[i].style.transform = "scale(" + newScale + ")";
+    //     }
+    // }
+
+    //FIX16 revert to original from FIX11
+     // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
+  function determineDx (elem, size) {
+    var oldWidth = elem.offsetWidth;
+    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
+    var oldSize = oldWidth / windowWidth;
+
+    // Changes the slider value to a percent width
+    function sizeSwitcher (size) {
+      switch(size) {
+        case "1":
+          return 0.25;
+        case "2":
+          return 0.3333;
+        case "3":
+          return 0.5;
+        default:
+          console.log("bug in sizeSwitcher");
+      }
     }
 
-    // READ and WRITE seperate tasks to avoid FSL
-    function changePizzaSizes(size) {
-        // READ - Load all pizza image elements
-        var element = document.querySelectorAll(".img-responsive");
-        // Sets value of newScale
-        var newScale = determineNewScale(size);
-        // WRITE - New transforms for pizza size updated here
-        for (var i = 0; i < element.length; i++) {
-            element[i].style.transform = "scale(" + newScale + ")";
-        }
+    var newSize = sizeSwitcher(size);
+    var dx = (newSize - oldSize) * windowWidth;
+
+    return dx;
+  }
+
+  // Iterates through pizza elements on the page and changes their widths
+  function changePizzaSizes(size) {
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
+  }
+
 
     changePizzaSizes(size);
 
